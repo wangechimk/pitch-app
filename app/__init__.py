3fcf87a8b0c75fc0c9b3845e66cae5e39a3cf5ed
wangechimk <wangechik.michelle@gmail.com>
2021-04-23 19:43:20 +0300

    fix navbar and add the config files

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from config import Config
+
 
 app = Flask(__name__)
 
 
 def create_app():
+    app.config.from_object(Config)
+    # from .auth import auth as authentication_blueprint
+    # from .main import main as main_blueprint
+    #
+    # app.register_blueprint(authentication_blueprint)
+    # app.register_blueprint(main_blueprint)
+
     return app
