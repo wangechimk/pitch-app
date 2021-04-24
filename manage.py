0745f043c030a0ffaf893c2e391e60b71b591442
wangechimk <wangechik.michelle@gmail.com>
2021-04-24 21:22:07 +0300

    add user test to the model

--- manage.py
+++ manage.py
@@ -17,5 +17,12 @@ def make_shell_context():
     return dict(app=app, db=db, User=User)
 
 
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == "__main__":
     manager.run()
