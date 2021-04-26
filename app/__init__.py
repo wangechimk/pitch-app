076f94461619d9a69ed20570c0cbe519f7bba54e
wangechimk <wangechik.michelle@gmail.com>
2021-04-27 00:15:08 +0300

    connecting the gmail account to the smtp server

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -13,6 +14,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.load_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 
 def create_app(config_name):
@@ -28,5 +30,6 @@ def create_app(config_name):
     bootstrap.init_app(app)
     login_manager.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
 
     return app
